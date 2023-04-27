4eb3992a39081f749cc999dae68876adcfe65c88
raluciure <ralu.ciure@gmail.com>
2023-04-27 16:45:05 +0200

    added buy page

--- src/app/buy-page/buy-page.component.ts
+++ src/app/buy-page/buy-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-buy-page',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,) { }
 
   ngOnInit(): void {
   }
+
+  toUnimplemented() {
+    this.router.navigate([`/unimplemented`]);
+  } 
 
 }
